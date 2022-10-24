e8779c5bc2cca9ef428ba65d5e799e6d40fe3de2
miguelovila <miguelovila@gmail.com>
2022-10-24 11:21:51 +0100

    Handle command, arguments and custom flags

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { app } from "electron";
+import { Command } from "commander";
+
+import { name, description, version } from "../package.json";
+import webapp from "./webapp";
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+  process.exit(0);
+}
+
+const program = new Command();
+
+program
+  .name(name)
+  .description(description)
+  .version(version);
+program
+  .command("open")
+  .description("Open a webapp in a new normal window")
+  .argument("<url>", "The URL to open")
+  .option("-ua, --user-agent <userAgent>", "The user agent to use")
+  .option("-w, --width <width>", "The width of the window")
+  .option("-h, --height <height>", "The height of the window")
+  .option("-t, --title <title>", "The title of the window")
+  .action((url, options) => {
+    app.whenReady().then(() => {
+
+      options.userAgent = options.userAgent || "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
+      options.width = parseInt(options.width) || 1000;
+      options.height = parseInt(options.height) || 600;
+
+      const webappInstance = new webapp(url, options);
+    });
+  });
+
+program.parse(process.argv);
---
+++ src/webapp.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron';
+
+export default class webapp {
+
+  private readonly window: BrowserWindow;
+
+  constructor(url: string, options: any) {
+    this.window = new BrowserWindow({
+      title: options.title,
+      width: options.width,
+      height: options.height,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    if (options.title) {
+      this.window.setTitle(options.title);
+      this.window.on('page-title-updated', function(e) {
+        e.preventDefault()
+      });
+    }
+    this.window.loadURL(url, { userAgent: options.userAgent });
+  }
+
+};
